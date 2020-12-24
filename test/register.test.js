aeab7c62bafdb94d4f87482dc63a4128b320ab2f
itsmeacs01 <itsmeacs01@gmail.com>
2020-12-24 19:35:46 +0545

    login test is done

--- test/register.test.js
+++ test/register.test.js
@@ -3,13 +3,18 @@ const expect = require('chai').expect;
 const app = require('../app');
 const User = require('../model/userModel');
 
-const userData = {
+const userRegisterData = {
   email: 'itsmeacs01@gmail.com',
   userName: 'Aashish Pokhrel',
   password: 'testtesttest',
   confirmPassword: 'testtesttest',
   role: 'employer',
   image: '../public/img/1500x500.jpeg-1608790415025.jpeg',
+};
+
+const userLoginData = {
+  email: 'itsmeacs01@gmail.com',
+  password: 'testtesttest',
 };
 
 describe('Signup', () => {
@@ -20,11 +25,24 @@ describe('Signup', () => {
   it('Should Signup Successfully', () => {
     return request(app)
       .post('/api/users/register')
-      .send(userData)
+      .send(userRegisterData)
       .expect(200)
       .then((res) => {
-          const body = res.body;
+        const body = res.body;
         expect(body.message).equal('success');
+      });
+  });
+});
+
+describe('Login', () => {
+  it('Should login Successfully', () => {
+    return request(app)
+      .post('/api/users/login')
+      .send(userLoginData)
+      .expect(200)
+      .then((res) => {
+        const body = res.body;
+        expect(body.isLoggedIn).equal(true);
       });
   });
 });
